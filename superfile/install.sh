ca57946e8b2ed36477cea49aa4c3496f60d0153d
Valdemar Jr <valdemarjuniorr@gmail.com>
2025-10-08 11:18:06 -0300

    change to check if superfile is already installed. change to everforest-dark-medium themere as default

--- superfile/install.sh
+++ superfile/install.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
 # Create a dotfile installation for linux and macOS
-bash -c "$(curl -sLo- https://superfile.dev/install.sh)"
+set -e
+if command -v spf &>/dev/null; then
+	echo "superfile is already installed."
+else
+	echo "Installing superfile..."
+	bash -c "$(curl -sLo- https://superfile.dev/install.sh)"
+fi
+
+# Detect OS
+OS="$(uname)"
+
+if [[ "$OS" == "Linux" ]]; then
+	ln -sf $(pwd)/config.toml ~/.config/superfile/config.toml
+elif [[ "$OS" == "Darwin" ]]; then
+	ln -sf $(pwd)/config.toml ~/Library/Application Support/superfile/config.toml
+else
+	echo "Unsupported OS: $OS"
+	exit 1
+fi
